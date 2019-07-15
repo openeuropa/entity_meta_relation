01c2c3f6159d5549afdfeb14b90c3e7b7aff714e
Hernani Borges de Freitas <hernani@Hernanis-MacBook-Air.local>
2019-07-15 17:31:31 +0100

    OPENEUROPA-1972: Add functional tests.

---
+++ tests/Functional/EntityMetaRelationContentFormTest.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\entity_meta_relation\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the content form flow.
+ */
+class EntityMetaRelationContentFormTest extends BrowserTestBase {
+
+  /**
+   * Admin user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $adminUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'path',
+    'node',
+    'user',
+    'system',
+    'workflows',
+    'emr',
+    'entity_meta_example',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->adminUser = $this->drupalCreateUser([
+      'access administration pages',
+      'administer content types',
+      'administer nodes',
+      'edit any entity_meta_example_ct content',
+      'edit own entity_meta_example_ct content',
+      'access content overview',
+    ]);
+
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests the entity version field behaviour can be configured per transition.
+   */
+  public function testContentWithEntityMetaEditing(): void {
+    $emr_manager = \Drupal::service('emr.manager');
+
+    // Saves new content entity.
+    $label = 'Node example';
+    $this->drupalGet('node/add/entity_meta_example_ct');
+    $this->getSession()->getPage()->fillField('Title', $label);
+    $this->getSession()->getPage()->uncheckField('Published');
+    $this->getSession()->getPage()->fillField('Color', 'red');
+    $this->getSession()->getPage()->pressButton('Save');
+
+    $this->getSession()->getPage()->hasContent("{$label} has been created");
+    $node = $this->getOneEntityByLabel('node', $label);
+    // Checks if the related entity meta have been properly created.
+    $entity_meta_relations = $emr_manager->getRelatedEntityMeta($node);
+    $this->assertNotEmpty($entity_meta_relations);
+    foreach ($entity_meta_relations as $entity_meta) {
+      // Color was properly saved.
+      $this->assertEquals($entity_meta->get('field_color')->value, 'red');
+      // Status was properly set.
+      $this->assertFalse($entity_meta->get('status')->value);
+      $oldRevisionId = $entity_meta->getRevisionId();
+    }
+
+    $this->drupalGet('node/' . $node->id() . '/edit');
+    $this->getSession()->getPage()->fillField('Title', 'Node example 2');
+    $this->getSession()->getPage()->checkField('Published');
+    $this->getSession()->getPage()->selectFieldOption('Color', 'green');
+    $this->getSession()->getPage()->pressButton('Save');
+
+    $node = $this->getOneEntityByLabel('node', 'Node example 2');
+    $entity_meta_relations = $emr_manager->getRelatedEntityMeta($node);
+
+    $this->assertNotEmpty($entity_meta_relations);
+    foreach ($entity_meta_relations as $entity_meta) {
+      $oldRevisionId = $entity_meta->getRevisionId();
+      // Color was properly saved.
+      $this->assertEquals($entity_meta->get('field_color')->value, 'green');
+      // Status was properly set.
+      $this->assertTrue($entity_meta->get('status')->value);
+    }
+
+    // @TODO: Test situation of no changes.
+  }
+
+  /**
+   * Loads a single entity by its label.
+   *
+   * @param string $type
+   *   The type of entity to load.
+   * @param string $label
+   *   The label of the entity to load.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface
+   *   The entity.
+   */
+  protected function getOneEntityByLabel($type, $label) {
+    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
+    $entity_type_manager = \Drupal::service('entity_type.manager');
+    $property = $entity_type_manager->getDefinition($type)->getKey('label');
+    $entity_list = $entity_type_manager->getStorage($type)->loadByProperties([$property => $label]);
+    $entity = current($entity_list);
+    if (!$entity) {
+      $this->fail("No {$type} entity named {$label} found.");
+    }
+
+    return $entity;
+  }
+
+}
