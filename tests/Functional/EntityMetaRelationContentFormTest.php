27b889b3ca5b884c099497a22213b4c3df75e90c
Hernâni <hernaniborgesdefreitas@gmail.com>
2020-02-25 17:39:09 +0000

    OPENEUROPA-2276: Added more test coverage.

--- tests/Functional/EntityMetaRelationContentFormTest.php
+++ tests/Functional/EntityMetaRelationContentFormTest.php
@@ -47,6 +47,8 @@ class EntityMetaRelationContentFormTest extends BrowserTestBase {
       'administer nodes',
       'edit any entity_meta_example_ct content',
       'edit own entity_meta_example_ct content',
+      'edit any entity_meta_multi_example_ct content',
+      'edit own entity_meta_multi_example_ct content',
       'access content overview',
     ]);
 
@@ -54,9 +56,9 @@ class EntityMetaRelationContentFormTest extends BrowserTestBase {
   }
 
   /**
-   * Tests the entity version field behaviour can be configured per transition.
+   * Tests content editing in entities using a single entity meta.
    */
-  public function testContentWithEntityMetaEditing(): void {
+  public function testContentWithSingleEntityMetaEditing(): void {
     /** @var \Drupal\emr\EntityMetaStorageInterface $entity_meta_storage */
     $entity_meta_storage = $this->container->get('entity_type.manager')->getStorage('entity_meta');
     /** @var \Drupal\emr\EntityMetaRelationStorageInterface $entity_meta_relation_storage */
@@ -80,6 +82,8 @@ class EntityMetaRelationContentFormTest extends BrowserTestBase {
     $this->getSession()->getPage()->fillField('Title', $label);
     $this->getSession()->getPage()->uncheckField('Published');
     $this->getSession()->getPage()->fillField('Color', 'red');
+    $this->assertFalse($this->getSession()->getPage()->hasField('Volume'));
+    $this->assertFalse($this->getSession()->getPage()->hasField('Gear'));
     $this->getSession()->getPage()->pressButton('Save');
 
     $this->getSession()->getPage()->hasContent("{$label} has been created");
@@ -102,6 +106,8 @@ class EntityMetaRelationContentFormTest extends BrowserTestBase {
     $this->getSession()->getPage()->fillField('Title', 'Node example 2');
     $this->getSession()->getPage()->checkField('Published');
     $this->getSession()->getPage()->selectFieldOption('Color', 'green');
+    $this->assertFalse($this->getSession()->getPage()->hasField('Volume'));
+    $this->assertFalse($this->getSession()->getPage()->hasField('Gear'));
     $this->getSession()->getPage()->pressButton('Save');
     $node_updated = $this->getEntityByLabel('node', 'Node example 2');
     $entity_meta_relation_storage->resetCache();
@@ -143,6 +149,8 @@ class EntityMetaRelationContentFormTest extends BrowserTestBase {
     $this->getSession()->getPage()->fillField('Title', 'Node example 4');
     $this->getSession()->getPage()->uncheckField('Create new revision');
     $this->getSession()->getPage()->selectFieldOption('Color', 'red');
+    $this->assertFalse($this->getSession()->getPage()->hasField('Volume'));
+    $this->assertFalse($this->getSession()->getPage()->hasField('Gear'));
     $this->getSession()->getPage()->pressButton('Save');
     $node_updated_no_revision = $this->getEntityByLabel('node', 'Node example 4');
     $entity_meta_relation_storage->resetCache();
@@ -236,6 +244,154 @@ class EntityMetaRelationContentFormTest extends BrowserTestBase {
     $entity_meta = reset($entity_meta_entities);
     $this->assertEquals(1, $entity_meta->id());
     $this->assertEquals($entity_meta->get('field_color')->value, 'red');
+  }
+
+  /**
+   * Tests content editing in entities using multiple entity metas.
+   */
+  public function testContentWithMultiEntityMetaEditing(): void {
+    /** @var \Drupal\emr\EntityMetaStorageInterface $entity_meta_storage */
+    $entity_meta_storage = $this->container->get('entity_type.manager')->getStorage('entity_meta');
+    /** @var \Drupal\emr\EntityMetaRelationStorageInterface $entity_meta_relation_storage */
+    $entity_meta_relation_storage = $this->container->get('entity_type.manager')->getStorage('entity_meta_relation');
+    /** @var \Drupal\node\NodeStorageInterface $node_storage */
+    $node_storage = $this->container->get('entity_type.manager')->getStorage('node');
+
+    // Create a new content entity but don't specify any meta value.
+    $this->drupalGet('node/add/entity_meta_multi_example_ct');
+    $this->getSession()->getPage()->fillField('Title', 'No meta node');
+    $this->getSession()->getPage()->uncheckField('Published');
+    $this->getSession()->getPage()->pressButton('Save');
+    $this->getSession()->getPage()->hasContent("No meta node has been created");
+    // No meta entities or meta entity relation entities should be created.
+    $entity_meta_storage->resetCache();
+    $entity_meta_relation_storage->resetCache();
+    $this->assertEmpty($entity_meta_storage->loadMultiple());
+    $this->assertEmpty($entity_meta_relation_storage->loadMultiple());
+
+    // Create a new content entity with several meta values.
+    $label = 'Node example';
+    $this->drupalGet('node/add/entity_meta_multi_example_ct');
+    $this->getSession()->getPage()->fillField('Title', $label);
+    $this->getSession()->getPage()->uncheckField('Published');
+    $this->getSession()->getPage()->fillField('Color', 'red');
+    $this->getSession()->getPage()->fillField('Volume', 'low');
+    $this->getSession()->getPage()->fillField('Gear', '2');
+    $this->getSession()->getPage()->pressButton('Save');
+
+    $this->getSession()->getPage()->hasContent("{$label} has been created");
+    $node = $this->getEntityByLabel('node', $label);
+    // Checks if the related entity metas have been properly created.
+    $entity_meta_entities = $entity_meta_storage->getRelatedEntities($node);
+    $this->assertCount(3, $entity_meta_entities);
+    // Entity meta relations entity should exist.
+    $this->assertCount(3, $entity_meta_relation_storage->loadMultiple());
+
+    $audio_meta = $node->get('emr_entity_metas')->getEntityMeta('audio');
+    $visual_meta = $node->get('emr_entity_metas')->getEntityMeta('visual');
+    $speed_meta = $node->get('emr_entity_metas')->getEntityMeta('speed');
+
+    // Visual was properly saved.
+    $this->assertEquals($visual_meta->get('field_color')->value, 'red');
+    $this->assertFalse($visual_meta->get('status')->value);
+
+    // Audio was properly saved.
+    $this->assertEquals($audio_meta->get('field_volume')->value, 'low');
+    $this->assertFalse($audio_meta->get('status')->value);
+
+    // Speed was properly saved.
+    $this->assertEquals($speed_meta->getWrapper()->getGear(), '2');
+    $this->assertFalse($speed_meta->get('status')->value);
+
+    // Change node status and color.
+    $this->drupalGet('node/' . $node->id() . '/edit');
+    $this->getSession()->getPage()->fillField('Title', 'Node example 2');
+    $this->getSession()->getPage()->checkField('Published');
+    $this->getSession()->getPage()->selectFieldOption('Color', 'green');
+    $this->getSession()->getPage()->pressButton('Save');
+
+    // Reset static caches.
+    $entity_meta_relation_storage->resetCache();
+    $entity_meta_storage->resetCache();
+    $node_storage->resetCache();
+
+    $node_updated = $this->getEntityByLabel('node', 'Node example 2');
+    $entity_meta_entities = $entity_meta_storage->getRelatedEntities($node_updated);
+    $this->assertCount(3, $entity_meta_entities);
+    // Three entity meta relation entities should exist.
+    $this->assertCount(3, $entity_meta_relation_storage->loadMultiple());
+
+    $audio_meta = $node_updated->get('emr_entity_metas')->getEntityMeta('audio');
+    $visual_meta = $node_updated->get('emr_entity_metas')->getEntityMeta('visual');
+    $speed_meta = $node_updated->get('emr_entity_metas')->getEntityMeta('speed');
+
+    // Visual was properly saved.
+    $this->assertEquals($visual_meta->get('field_color')->value, 'green');
+    $this->assertTrue($visual_meta->get('status')->value);
+
+    // Audio was properly saved.
+    $this->assertEquals($audio_meta->get('field_volume')->value, 'low');
+    $this->assertTrue($audio_meta->get('status')->value);
+
+    // Speed was properly saved.
+    $this->assertEquals($speed_meta->getWrapper()->getGear(), '2');
+    $this->assertTrue($speed_meta->get('status')->value);
+
+    // Revision changed.
+    $this->assertEquals(4, $visual_meta->getRevisionId());
+
+    // Do not save color but update the node.
+    $this->drupalGet('node/' . $node->id() . '/edit');
+    $this->getSession()->getPage()->fillField('Title', 'Node example 3');
+    $this->getSession()->getPage()->pressButton('Save');
+    $node_updated_no_meta_changes = $this->getEntityByLabel('node', 'Node example 3');
+    $entity_meta_relation_storage->resetCache();
+    $entity_meta_storage->resetCache();
+    $node_storage->resetCache();
+    $entity_meta_entities = $entity_meta_storage->getRelatedEntities($node_updated_no_meta_changes);
+
+    $this->assertCount(3, $entity_meta_entities);
+    $this->assertCount(3, $entity_meta_relation_storage->loadMultiple());
+
+    $audio_meta = $node_updated_no_meta_changes->get('emr_entity_metas')->getEntityMeta('audio');
+    $visual_meta = $node_updated_no_meta_changes->get('emr_entity_metas')->getEntityMeta('visual');
+    $speed_meta = $node_updated_no_meta_changes->get('emr_entity_metas')->getEntityMeta('speed');
+
+    // Color was kept the same.
+    $this->assertEquals($visual_meta->get('field_color')->value, 'green');
+    $this->assertTrue($visual_meta->get('status')->value);
+    // Audio was properly saved.
+    $this->assertEquals($audio_meta->get('field_volume')->value, 'low');
+    $this->assertTrue($audio_meta->get('status')->value);
+    // Speed was properly saved.
+    $this->assertEquals($speed_meta->getWrapper()->getGear(), '2');
+    $this->assertTrue($speed_meta->get('status')->value);
+
+    // Revision did not change.
+    $this->assertEquals(4, $visual_meta->getRevisionId());
+
+    // Do not create a new revision but change color.
+    $this->drupalGet('node/' . $node->id() . '/edit');
+    $this->getSession()->getPage()->fillField('Title', 'Node example 4');
+    $this->getSession()->getPage()->uncheckField('Create new revision');
+    $this->getSession()->getPage()->selectFieldOption('Color', 'red');
+    $this->getSession()->getPage()->pressButton('Save');
+    $node_updated_no_revision = $this->getEntityByLabel('node', 'Node example 4');
+    $entity_meta_relation_storage->resetCache();
+    $entity_meta_storage->resetCache();
+    $node_storage->resetCache();
+    $entity_meta_entities = $entity_meta_storage->getRelatedEntities($node_updated_no_revision);
+
+    // Checks we keep having three entity metas.
+    $this->assertCount(3, $entity_meta_entities);
+    // Three entity meta relations entities should exist.
+    $this->assertCount(3, $entity_meta_relation_storage->loadMultiple());
+    $visual_meta = $node_updated_no_revision->get('emr_entity_metas')->getEntityMeta('visual');
+
+    // Color was changed.
+    $this->assertEquals($visual_meta->get('field_color')->value, 'red');
+    // Revision not changed.
+    $this->assertEquals(4, $visual_meta->getRevisionId());
   }
 
   /**
