b6fb5e290c757b79306e43f6095cb181a0a80710
Hernâni <hernaniborgesdefreitas@gmail.com>
2020-03-25 17:49:41 +0000

    OPENEUROPA-3005: Add entity_meta_force to test attach_by_default.

---
+++ modules/entity_meta_example/modules/entity_meta_force/src/Plugin/EntityMetaRelation/ForceConfiguration.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\entity_meta_force\Plugin\EntityMetaRelation;
+
+use Drupal\emr\Plugin\EntityMetaRelationInlineContentFormPluginBase;
+
+/**
+ * Plugin implementation of the entity_meta_relation.
+ *
+ * @EntityMetaRelation(
+ *   id = "force_configuration",
+ *   label = @Translation("Force configuration"),
+ *   entity_meta_bundle = "force",
+ *   content_form = TRUE,
+ *   attach_by_default = TRUE,
+ *   entity_meta_wrapper_class = "\Drupal\entity_meta_force\ForceEntityMetaWrapper",
+ *   description = @Translation("Force configuration.")
+ * )
+ */
+class ForceConfiguration extends EntityMetaRelationInlineContentFormPluginBase {}
